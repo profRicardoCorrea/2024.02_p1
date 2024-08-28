96217983ec669300090bc2bd87d574301a933abe
profRicardoCorrea <ricardo.correia@unifacol.edu.br>
2024-08-28 19:11:21 -0300

    UP

---
+++ src/basico/Variavel.java
@@ -0,0 +1,26 @@
+package basico;
+
+public class Variavel {
+	
+	public static void main(String []args) {
+		/*String nome="trert vsdff";
+		int numero=12;
+		float moeda=12.56f;
+		boolean isFrio=false;
+		char sexo='M';
+		char sexo1='B';
+		char sexo2='C';
+		System.out.println('>'+nome);
+		System.out.println(numero);*/
+		int x=5;
+		System.out.println(x);		 
+		System.out.println(++x);
+		System.out.println(x);	
+		String nome="fulanofulanofulanofulanofulanofulanofulanofulanofulanofulanofulanofulanofulanofulanofulanofulano";
+		System.out.println(nome.length());
+		System.out.println(nome.toUpperCase());
+		
+		 
+	}
+
+}
